485309c85e4bf3422752890a088ace2b2e94656b
Christine Ronk <35753005+cmronk@users.noreply.github.com>
2018-06-26 10:07:49 -0500

    Add files via upload
    Customer view is working correctly

---
+++ bamazonCustomer.js
@@ -0,0 +1,110 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazonDB"
+});
+
+console.log("Welcome to BAMAZON! See our current list of products below.  Choose items by Item ID.  Happy Shopping!!!");
+function displayItems() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        var table = new Table({
+            head: ["ID", "Description", "Department", "Price"],
+            colWidths: [5, 35, 20, 10]
+        });
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price]);
+        }
+        console.log(table.toString());
+
+        shoppingCart();
+    });
+};
+
+function shoppingCart() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "itemID",
+            message: "What would you like to purchase? (Use Item ID to select.)",
+            choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+        },
+        {
+            type: "confirm",
+            name: "confirmItem",
+            message: "Are you sure you want to purchase this?"
+        },
+        {
+            type: "input",
+            name: "quantityRequested",
+            message: "How many would you like sir???"
+        }
+
+    ]).then(function (userAnswers) {
+        connection.query("SELECT * FROM products", function (err, res) {
+            if (err) throw err;
+            // need to figure out how to not ask quantity once user selects no
+            if (userAnswers.confirmItem === false) {
+                console.log(userAnswers.confirmItem);
+                endShopping();
+            }
+            // checks to see if quantity is in stock.  using -1 to get correct index
+            else if (userAnswers.quantityRequested < res[(userAnswers.itemID) - 1].stock_quantity) {
+                console.log("You have purchased " + userAnswers.quantityRequested + " of " + userAnswers.itemID + ".");
+                // calculates total plus taxes
+                console.log("Your total is $" + ((userAnswers.quantityRequested * res[(userAnswers.itemID) - 1].price) * 0.0825 + (userAnswers.quantityRequested * res[(userAnswers.itemID) - 1].price)).toFixed(2) + ".");
+                var query = connection.query(
+
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: (res[(userAnswers.itemID) - 1].stock_quantity) - userAnswers.quantityRequested
+                        },
+                        {
+                            item_id: userAnswers.itemID
+                        }
+                    ],
+                )
+                "SELECT * FROM products"
+
+                query.sql;
+
+                endShopping();
+            } else {
+                console.log("Sorry, we do not have the selected quantity in stock.  Please try another item or less of your chosen item.");
+                endShopping();
+            }
+        })
+    });
+}
+
+connection.connect(function (err) {
+    if (err) throw err;
+    displayItems();
+});
+
+
+
+
+function endShopping() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "quit",
+            message: "Would you like to continue shopping?"
+        }
+    ]).then(function (userAnswer) {
+        if (userAnswer.quit === true) {
+            shoppingCart();
+        } else {
+            connection.end();
+        }
+    })
+};
+
